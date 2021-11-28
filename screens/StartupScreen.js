fdd8a4f502b2a2c3b34bfb2b1f0964432fcbf305
Abhijeet Kumar <63132585+Abhijeet1520@users.noreply.github.com>
2021-11-29 00:20:04 +0530

    Add StartupScreen

---
+++ screens/StartupScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+class StartupScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Welcome to Gamla Funds </Text>
+      </View>
+    );
+  }
+}
+
+export { StartupScreen };
